4484f842495e92128781f88180bf4b3477b15912
Mubaraq Onipede <93594617+Mubarraqqq@users.noreply.github.com>
2024-03-14 16:02:04 +0100

    Update Aviation API.py

--- Aviation API.py
+++ Aviation API.py
@@ -22,7 +22,12 @@ for j, i in enumerate(codes):
 
     # Define a function to fetch data from the API based on the airport code
     def file_path_():    
-        # Construct the URL for API request using the airport code
+        '''
+        The function file_path_() fetches departure and arrival data for a given airport code from an aviation API. 
+        It constructs the API URL, sends a request, parses the response, and returns the departure and arrival data as Pandas DataFrames. 
+        It handles rate limiting and prints error messages if the request fails.
+        '''
+        
         url = f'https://api.aviationapi.com/v1/vatsim/pilots?apt=K{i}'
         
         # Send HTTP GET request to the API endpoint with a timeout of 10 seconds
@@ -32,32 +37,24 @@ for j, i in enumerate(codes):
         if j % 40 == 0:
             time.sleep(20)
             
-        # Initialize DataFrames to store departures and arrivals
         df_departures = pd.DataFrame()
         df_arrivals = pd.DataFrame()
         
-        # Check if the request was successful (status code 200)
         if response.status_code == 200:
             # Parse the JSON response
             data = response.json()
             try:
-                # Extract departures and arrivals data from the JSON response
                 df_departures = pd.DataFrame(data[i]['Departures'])
                 df_arrivals = pd.DataFrame(data[i]['Arrivals'])
-            except KeyError:
-                # Handle the case where the 'Departures' or 'Arrivals' key is missing
+            except KeyError: # Handle the case where the 'Departures' or 'Arrivals' key is missing
                 pass
         else:
-            # Print an error message if the request fails
             print(f"Failed to fetch data. Status code: {response.status_code}")
         
-        # Return the DataFrames containing departures and arrivals data
         return df_departures, df_arrivals
     
-    # Call the function to fetch data for the current airport code
     A, B = file_path_()
 
-    # Concatenate the departures and arrivals data with the total departures and arrivals DataFrames
     Total_departures = pd.concat([Total_departures, A], ignore_index=True)
     Total_arrival = pd.concat([Total_arrival, B], ignore_index=True)
 
